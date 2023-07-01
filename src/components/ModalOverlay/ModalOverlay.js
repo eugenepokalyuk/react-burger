de17caace4534ba99dca509b2dd44c256b3c6383
eugenepokalyuk <eugene.pokalyuk@gmail.com>
2023-07-01 17:13:04 +0700

    Update ModalOverlay.js
    Calling onClose on click

--- src/components/ModalOverlay/ModalOverlay.js
+++ src/components/ModalOverlay/ModalOverlay.js
@@ -3,7 +3,9 @@ import PropTypes from 'prop-types'
 import styles from './ModalOverlay.module.css'
 
 const ModalOverlay = ({ onClose }) => {
-    return (<div className={styles.overlay}></div>)
+    return (
+        <div className={styles.overlay} onClick={onClose}></div>
+    )
 }
 
 ModalOverlay.propTypes = {
