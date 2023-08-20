368ec6c82b5215b7b56018531b41ae79d475f53f
eugenepokalyuk <eugene.pokalyuk@gmail.com>
2023-08-20 22:52:54 +0700

    Update ingredientCounter.ts
    Типизация экшенов

--- src/services/actions/ingredientCounter.ts
+++ src/services/actions/ingredientCounter.ts
@@ -1,8 +1,18 @@
 // нужен ли мне этот экшн?
 
-export const UPDATE_INGREDIENT_COUNT = 'UPDATE_INGREDIENT_COUNT' as const;
+export const UPDATE_INGREDIENT_COUNT: 'UPDATE_INGREDIENT_COUNT' = 'UPDATE_INGREDIENT_COUNT' as const;
 
-export const updateIngredientCount = (ingredientId: string, count: number) => {
+export type TIngredientCounter =
+    | IUpdateIngredientCountAction;
+
+export interface IUpdateIngredientCountAction {
+    readonly type: typeof UPDATE_INGREDIENT_COUNT;
+    readonly payload: {
+        ingredientId: string, count: number
+    }
+}
+
+export const updateIngredientCount = (ingredientId: string, count: number): IUpdateIngredientCountAction => {
     return {
         type: UPDATE_INGREDIENT_COUNT,
         payload: {
