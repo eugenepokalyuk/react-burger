f479a6f83d3215c32ac149556e003a049b94bdac
eugenepokalyuk <eugene.pokalyuk@gmail.com>
2023-08-20 22:52:31 +0700

    Update currentIngredient.ts
    Типизация экшенов
    Типизация генераторов экшена

--- src/services/actions/currentIngredient.ts
+++ src/services/actions/currentIngredient.ts
@@ -1,13 +1,28 @@
 import { Ingredient } from "../types";
 
-export const ADD_VIEWED_INGREDIENT = 'ADD_VIEWED_INGREDIENT' as const;
-export const CLEAR_VIEWED_INGREDIENT = 'CLEAR_VIEWED_INGREDIENT' as const;
+export const ADD_VIEWED_INGREDIENT: 'ADD_VIEWED_INGREDIENT' = 'ADD_VIEWED_INGREDIENT' as const;
+export const CLEAR_VIEWED_INGREDIENT: 'CLEAR_VIEWED_INGREDIENT' = 'CLEAR_VIEWED_INGREDIENT' as const;
 
-export const addViewedIngredient = (ingredient: Ingredient) => ({
+export type TCurrentIngredient =
+    | IAddViewedIngredientAction
+    | IClearViewedIngredientAction;
+
+// Генераторы экшенов
+
+export interface IAddViewedIngredientAction {
+    readonly type: typeof ADD_VIEWED_INGREDIENT;
+    readonly payload: Ingredient;
+}
+
+export interface IClearViewedIngredientAction {
+    readonly type: typeof CLEAR_VIEWED_INGREDIENT;
+}
+
+export const addViewedIngredient = (ingredient: Ingredient): IAddViewedIngredientAction => ({
     type: ADD_VIEWED_INGREDIENT,
     payload: ingredient,
 });
 
-export const clearViewedIngredient = () => ({
+export const clearViewedIngredient = (): IClearViewedIngredientAction => ({
     type: CLEAR_VIEWED_INGREDIENT,
 });
