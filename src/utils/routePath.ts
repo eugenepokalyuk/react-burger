c48499e4d6a0d5f9411e8c8836c1278ee5180594
eugenepokalyuk <eugene.pokalyuk@gmail.com>
2023-09-02 05:12:27 +0700

    Update routePath.ts
    Update feed :id => :number

--- src/utils/routePath.ts
+++ src/utils/routePath.ts
@@ -6,10 +6,10 @@ export const RESET_PASSWORD_PATH = "/reset-password";
 
 export const PROFILE_PATH = "/profile";
 export const PROFILE_ORDERS_PATH = "/profile/orders"; // страница истории заказов пользователя. Доступен только авторизованным пользователям.
-export const PROFILE_ORDERS_ID_PATH = "/profile/orders/:id"; // страница заказа в истории заказов. Доступен только авторизованным пользователям.
+export const PROFILE_ORDERS_ID_PATH = "/profile/orders/:number"; // страница заказа в истории заказов. Доступен только авторизованным пользователям.
 
 export const FEED_PATH = "/feed"; // страница ленты заказов. Доступен всем пользователям.
-export const FEED_ID_PATH = "/feed/:id"; // страница заказа в ленте. Доступен всем пользователям.
+export const FEED_ID_PATH = "/feed/:number"; // страница заказа в ленте. Доступен всем пользователям.
 
 export const INGREDIENTS_PATH = "/ingredients/:id";
 export const ERROR_PATH = "*";
